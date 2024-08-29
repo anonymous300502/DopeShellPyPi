d45e986d742ad53965b2be48466e6621581b7d45
Manaswi Sharma <85053597+manaswii@users.noreply.github.com>
2024-08-29 19:46:05 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="DopeShell",
-    version="2.0.0",
+    version="2.0.1",
     author="Abhishek Sharma, Manaswi Sharma",
     author_email="170mailmea@gmail.com",
     description="A Python library for creating secure reverse shells with session management and encryption.",
